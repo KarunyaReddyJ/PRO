24c80eddfdb4019a62baa1997ab410fc3a05508e
KarunyaReddyJ <Karunyareddy1418@gmail.com>
2023-12-28 09:27:22 +0530

    nav bar is added with with responsive design

---
+++ script.js
@@ -0,0 +1,12 @@
+const tdash=document.getElementById("tdash")
+const navElements=document.querySelectorAll('.navA')
+let disp="none"
+tdash.addEventListener('click',()=>{
+    if(disp==="block")
+        disp="none"
+    else
+        disp="block"
+    navElements.forEach(element => {
+        element.style.display=disp
+    });
+})
